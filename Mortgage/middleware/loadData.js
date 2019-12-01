5591176ccc88ce0c027ed616ef81e7cfd8812931
beqa lazarashvili <beqalazarashvili@gmail.com>
2019-12-02 01:05:48 +0400

    update

--- Mortgage/middleware/loadData.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default function({params, store}) {
-    return axios.get(`https://api-squad1.livo.ge/v1/mortgage`)
-        .then((response) => {
-            store.commit('load', response.data.data)
-        });
-}
